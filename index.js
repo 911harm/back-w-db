80b8fd33b5456804c213e41bc0db803833d470a6
911harm <911harm@gmail.com>
2020-10-03 13:02:52 -0500

    try

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict'
 var mongoose= require('mongoose');
-var url='mongodb://localhost:27017/test1';
+var url= process.env.DB_URL || 'mongodb://localhost:27017/test1';
 var app= require('./app');
-var port =9300;
+var port =process.env.PORT || '9300';
 
 
 mongoose.Promise=global.Promise;//evita fallos, con las promesas

96e21cafdd0bb0c958a429c63d080fb8fc7c2208
911harm <911harm@gmail.com>
2020-10-03 13:12:35 -0500

    try 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var mongoose= require('mongoose');
 var url= process.env.DB_URL || 'mongodb://localhost:27017/test1';
 var app= require('./app');
 var port =process.env.PORT || '9300';
+const host=process.env.HOST|| '0.0.0.0';
 
 
 mongoose.Promise=global.Promise;//evita fallos, con las promesas
@@ -12,7 +13,7 @@ mongoose.set('useFindAndModify',false);//los nuevos metodos y evita los viejos
 mongoose.connect(url,{useNewUrlParser:true}).then(()=>{
     console.log("Conectado el Servidor");
     //crear servidor y escucha
-    app.listen(port,()=>{
-        console.log("Servidor corriendo en localhots:"+port)
+    app.listen(port,host,()=>{
+        console.log("Servidor corriendo en "+host+port)
     })
 });
